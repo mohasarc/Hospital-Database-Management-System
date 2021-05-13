77e75de3647adf5a226c5a32b9bcc77c6fd226da
melhamin <mo.elhamin@gmail.com>
2021-05-14 00:57:21 +0300

    add all tests and lab technician related endpoints

--- server/api/appointment/appointment.js
+++ server/api/appointment/appointment.js
@@ -1,32 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const moment = require("moment");
 const { v4: uuidv4 } = require("uuid");
-const { connection } = require('../../index');
+const { connection } = require("../../index");
 const { APPT_STATUS } = require("../../constants");
 
 /****************************************************/
 /*                    Appointment                   */
 /****************************************************/
 // Create an appointment
-router.post("/", (req, res) => { //! Probably we need to limit the number of appointments the patient can have to 1 per day?
-    // Collect tuple data
+router.post("/", (req, res) => {
+	//! Probably we need to limit the number of appointments the patient can have to 1 per day?
+	// Collect tuple data
 	const { p_id, d_id, description } = req.body;
-    const appt_id    = uuidv4();
-    const date       = moment(new Date()).format( "YYYY-MM-DD" );
-    const status     = APPT_STATUS.ONGOING;
-    const appt_tuple = [appt_id, d_id, p_id, date, status, description];
-    
-    // Prepare SQL
+	const appt_id = uuidv4();
+	const date = moment(new Date()).format("YYYY-MM-DD");
+	const status = APPT_STATUS.ONGOING;
+	const appt_tuple = [appt_id, d_id, p_id, date, status, description];
+
+	// Prepare SQL
 	const appt_sql = `INSERT INTO appointment(appt_id, d_id, p_id, date, status, description) VALUES(?)`;
 
-    // Perform SQL
+	// Perform SQL
 	connection.query(appt_sql, [appt_tuple], (err, results) => {
 		if (err) {
-            res.status(200).send(err);
-        } else {
-            res.status(200).send(results);
-        }
+			res.status(200).send(err);
+		} else {
+			res.status(200).send(results);
+		}
 	});
 });
 
@@ -37,10 +38,10 @@ router.get("/", (req, res) => {
 
 	connection.query(sql, (err, results) => {
 		if (err) {
-            res.status(200).send(err);
-        } else {
-            res.status(200).send(results);
-        }
+			res.status(200).send(err);
+		} else {
+			res.status(200).send(results);
+		}
 	});
 });
 
@@ -61,30 +62,32 @@ router.post("/diagnosis", async (req, res) => {
 
 	// Retrieve all tests' results for this appointment
 	connection.query(testFinalizedSql, async (err, result) => {
-        var canDiagnose = true;
+		var canDiagnose = true;
 
 		if (err) {
 			res.status(500).send(err);
 		} else {
 			// Check if all tests were finalized
-            for (let i = 0; i < result.length; i++) {
+			for (let i = 0; i < result.length; i++) {
 				if (result[i].status != TEST_STATUS.finalized) {
-					res.status(500).send("There are some tests that are not yet finalized!");
-                    canDiagnose = false;
-                    break;
+					res.status(500).send(
+						"There are some tests that are not yet finalized!"
+					);
+					canDiagnose = false;
+					break;
 				}
-            }
+			}
 
-            // finalize diagnozing disease
-            if (canDiagnose) {
-                connection.query(sql, [tuple], async (err, result) => {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.status(200).send(result);
-                    }
-                });
-            }
+			// finalize diagnozing disease
+			if (canDiagnose) {
+				connection.query(sql, [tuple], async (err, result) => {
+					if (err) {
+						res.status(500).send(err);
+					} else {
+						res.status(200).send(result);
+					}
+				});
+			}
 		}
 	});
 });
@@ -92,7 +95,7 @@ router.post("/diagnosis", async (req, res) => {
 // Get the diseases diagnosed for a particular appointment
 router.get("/diagnosis", (req, res) => {
 	const { appt_id } = req.body;
-	const sql 	  = `SELECT name FROM diagnosis WHERE appt_id='${appt_id}'`;
+	const sql = `SELECT name FROM diagnosis WHERE appt_id='${appt_id}'`;
 
 	connection.query(sql, (err, results) => {
 		if (err) {
@@ -109,22 +112,26 @@ router.get("/diagnosis", (req, res) => {
 // Add a symbtom
 router.post("/symptom", (req, res) => {
 	const { appt_id, name, description } = req.body;
-    const patient_symptoms_tuple = [appt_id, name, description];
-	const patient_symptoms_sql     = `INSERT INTO has_symptoms(appt_id, name, description) VALUES(?)`;
+	const patient_symptoms_tuple = [appt_id, name, description];
+	const patient_symptoms_sql = `INSERT INTO has_symptoms(appt_id, name, description) VALUES(?)`;
 
-	connection.query(patient_symptoms_sql, [patient_symptoms_tuple], (err, results) => {
-		if (err) {
-			res.status(200).send(err);
-		} else {
-			res.status(200).send(results);
+	connection.query(
+		patient_symptoms_sql,
+		[patient_symptoms_tuple],
+		(err, results) => {
+			if (err) {
+				res.status(200).send(err);
+			} else {
+				res.status(200).send(results);
+			}
 		}
-	});
+	);
 });
 
 // Get symptoms shared for a particular appointment
 router.get("/symptom", (req, res) => {
 	const { appt_id } = req.body;
-	const sql     = `SELECT name, description FROM has_symptoms WHERE appt_id='${appt_id}'`;
+	const sql = `SELECT name, description FROM has_symptoms WHERE appt_id='${appt_id}'`;
 
 	connection.query(sql, (err, results) => {
 		if (err) {
@@ -139,7 +146,8 @@ router.get("/symptom", (req, res) => {
 /*                       Tests                      */
 /****************************************************/
 // Assign test to technician
-router.post("/test", (req, res) => { //! TO BE TESTED !!!!
+router.post("/test", (req, res) => {
+	//! TO BE TESTED !!!!
 	const { t_id, appt_id } = req.body;
 
 	// get all technicians with the same qualification
@@ -156,13 +164,16 @@ router.post("/test", (req, res) => { //! TO BE TESTED !!!!
 		if (err) {
 			res.status(500).send(err);
 		} else {
-			// Get a random one 
-			const lt_id = results[(Math.random() * results.length) % results.length].lt_id;
+			// Get a random one
+			const lt_id =
+				results[(Math.random() * results.length) % results.length]
+					.lt_id;
 			const assignTestSql = `INSERT INTO assigned_test(lt_id, appt_id, t_id) VALUES (?)`;
 			const assignTestTuple = [lt_id, appt_id, t_id];
 			const initCompResultsSql = `INSERT INTO component_result(c_id, c_name, t_id, appt_id, score) VALUES (?)`;
-			
-			connection.query(componentNamesSql, (err, results) => { //? I HATE CALLBACKS :'(
+
+			connection.query(componentNamesSql, (err, results) => {
+				//? I HATE CALLBACKS :'(
 				if (err) {
 					res.status(500).send(err);
 				} else {
@@ -170,28 +181,44 @@ router.post("/test", (req, res) => { //! TO BE TESTED !!!!
 						if (err) {
 							res.status(500).send(err);
 						}
-						
+
 						// Assign lab technician
-						connection.query(assignTestSql, assignTestTuple, (err, results) => {
-							if (err) {
-								connection.rollback();
-								res.status(500).send(err);
-							} else {
-								// Initialize all components
-								results.map((component, i) => {
-									let initCompResultsTuple = [uuidv4(), component.c_name, t_id, appt_id, null];
-									connection.query(initCompResultsSql, initCompResultsTuple, (err, results) => {
-										if (err) {
-											connection.rollback();
-											res.status(500).send(err);
-										} else {
-											if (i == results.length - 1)
-												res.status(200).send({ results }); //! This probably won't always work correctly :D
-										}
+						connection.query(
+							assignTestSql,
+							assignTestTuple,
+							(err, results) => {
+								if (err) {
+									connection.rollback();
+									res.status(500).send(err);
+								} else {
+									// Initialize all components
+									results.map((component, i) => {
+										let initCompResultsTuple = [
+											uuidv4(),
+											component.c_name,
+											t_id,
+											appt_id,
+											null,
+										];
+										connection.query(
+											initCompResultsSql,
+											initCompResultsTuple,
+											(err, results) => {
+												if (err) {
+													connection.rollback();
+													res.status(500).send(err);
+												} else {
+													if (i == results.length - 1)
+														res.status(200).send({
+															results,
+														}); //! This probably won't always work correctly :D
+												}
+											}
+										);
 									});
-								});
+								}
 							}
-						});
+						);
 					});
 				}
 			});
@@ -199,4 +226,88 @@ router.post("/test", (req, res) => { //! TO BE TESTED !!!!
 	});
 });
 
-module.exports = router
+// Get all tests for a patient
+router.get("/tests/patient", (req, res) => {
+	const p_id = req.body.p_id;
+	const sql = `SELECT at.t_id, apt.p_id, at.appt_id, apt.date, at.status
+				FROM assigned_test AS at, appointment AS apt
+				WHERE apt.appt_id=at.appt_id AND apt.p_id='${p_id}'
+				ORDER BY apt.date DESC`;
+	connection.query(sql, (err, results) => {
+		if (err) res.status(500).send(err);
+		res.status(200).send(results);
+	});
+});
+
+// Get all testS for an appointment
+router.get("/tests/appt", (req, res) => {
+	const appt_id = req.body.appt_id;
+	const sql = `SELECT  apt.appt_id, apt.p_id, at.t_id, apt.date, at.status
+				FROM assigned_test AS at, appointment AS apt
+				WHERE apt.appt_id=at.appt_id and apt.appt_id='${appt_id}'`;
+
+	connection.query(sql, (err, results) => {
+		if (err) res.status(500).send(err);
+		res.status(200).send(results);
+	});
+});
+
+// Get all components for a test
+// with test id as t_id and appointment id as appt_id
+router.get("/tests/comps", (req, res, _) => {
+	const t_id = req.body.t_id;
+	const appt_id = req.body.appt_id;
+	const sql = `SELECT *
+				FROM component_result AS CR NATURAL JOIN components
+				WHERE CR.appt_id='${appt_id}' and CR.t_id='${t_id}'`;
+	connection.query(sql, (err, results) => {
+		if (err) res.status(500).send(err);
+		res.status(200).send(results);
+	});
+});
+
+// Get all past results for a component
+// using patient id, test id, comp. id, and
+// the current appiontment id that user has
+// selected component from
+router.get("/tests/patient/comps", (req, res) => {
+	const { p_id, t_id, c_id, appt_id } = req.body;
+	const sql = `SELECT CR.c_id, CR.t_id, AP.appt_id, CR.score, AP.date
+				FROM component_result AS CR, appointment AS AP
+				WHERE CR.appt_id=AP.appt_id AND AP.p_id='${p_id}' AND CR.t_id='${t_id}' AND CR.c_id='${c_id}' and CR.appt_id <> '${appt_id}'
+				ORDER BY AP.date DESC;`;
+
+	connection.query(sql, (err, results) => {
+		if (err) res.status(500).send(err);
+		res.status(200).send(results);
+	});
+});
+
+// Add result of a test component
+router.post("/tests/comps", (req, res) => {
+	const { t_id, c_id, appt_id, score } = req.body;
+	const values = [c_id, t_id, appt_id, score];
+	const sql = `INSERT INTO component_result(c_id, t_id, appt_id, score) VALUES(?);`;
+
+	connection.query(sql, [values], (err, results) => {
+		if (err) res.status(500).send(err);
+		res.status(200).send(results);
+	});
+});
+
+// Get all tests performed by the lab technician with given id
+router.get("/tests/lt", (req, res) => {
+	const lt_id = req.body.lt_id;
+	const sql = `SELECT *
+				FROM assigned_test NATURAL JOIN test
+				WHERE lt_id='${lt_id}'`;
+	connection.query(sql, (err, results) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(results);
+		}
+	});
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const { COMP_SCORE, TEST_STATUS } = require("./constants");
 const app = express();
 const PORT = 8000;
@@ -25,83 +25,14 @@ connection.connect((err) => {
 });
 
 // Redirect requests
-const authentication = require('./api/authentication/authentication');
-const appointment 	 = require('./api/appointment/appointment');
-const symptom 		 = require('./api/management/symptom');
-const disease 		 = require('./api/management/disease');
-const test			 = require('./api/management/test');
-app.use('/auth', authentication);
-app.use('/appointment', appointment);
-app.use('/management', symptom, disease, test);
-
-// Get all tests of the given patiant id
-app.get("/tests/:pid", (req, res, _) => {
-	const pid = req.params.pid;
-	const sql = `SELECT test.t_id, test.name, dv.date, test.status
-				FROM test, doc_visit AS dv
-				WHERE test.appt_id = dv.appt_id AND dv.p_id='${pid}'
-				ORDER BY dv.date DESC`;
-	connection.query(sql, (err, results) => {
-		if (err) throw err;
-		res.status(200).send(results);
-	});
-});
-
-// Get all components of the given test id
-app.get("/tests/comps/:t_id", (req, res, _) => {
-	const testID = req.params.t_id;
-	const sql = `SELECT T.name, C.c_name, C.score
-				FROM test AS T, components AS C
-				WHERE T.t_id = C.t_id AND T.t_id='${testID}'`;
-	connection.query(sql, (err, results) => {
-		if (err) throw err;
-		res.status(200).send(results);
-	});
-});
-
-// Get all tests performed by the lab technician with given id
-app.get("/tests/lt/:lt_id", (req, res) => {
-	const lt_id = req.params.lt_id;
-	const sql = `SELECT test.t_id, test.name, assigned_test.status
-				FROM assigned_test NATURAL JOIN test
-				where lt_id='${lt_id}'`;
-
-	connection.query(sql, (err, results) => {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.status(200).send(results);
-		}
-	});
-});
-
-// Update the score of a test component
-app.post("/tests/comps", (req, res) => {   //! NEEDS TO BE UPDATED ( I, mohammed, couldn't understand it ) @Elham
-	const { t_id, c_name, score } = req.body;
-
-	const sql1 = `UPDATE components
-				SET score='${score}'
-				WHERE c_name='${c_name}' and t_id='${t_id}'`;
-
-	const sql2 = `UPDATE test
-				SET status='${TEST_STATUS.finalized}'
-				WHERE t_id='${t_id}' and NOT EXISTS (
-					SELECT * FROM components WHERE t_id='${t_id}' and score='${COMP_SCORE.preparing}')`;
-
-	connection.beginTransaction((err) => {
-		if (err) {
-			res.status(500).send("SOMETHING WENT WRONG!");
-		}
-		connection.query(sql1, (err, results) => {
-			if (err) connection.rollback();
-		});
-
-		connection.query(sql2, (err, results) => {
-			if (err) connection.rollback();
-			res.status(200).send({ results });
-		});
-	});
-});
+const authentication = require("./api/authentication/authentication");
+const appointment = require("./api/appointment/appointment");
+const symptom = require("./api/management/symptom");
+const disease = require("./api/management/disease");
+const test = require("./api/management/test");
+app.use("/auth", authentication);
+app.use("/appointment", appointment);
+app.use("/management", symptom, disease, test);
 
 app.listen(PORT, () => {
 	console.log(`Listening on PORT: ${PORT}`);
