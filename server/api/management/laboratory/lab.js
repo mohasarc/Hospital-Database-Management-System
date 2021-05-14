6586e47176f2ab5b542b7018f554cb2ceaf95c50
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2021-05-15 02:06:41 +0300

    implemented lab crd ops

--- server/api/management/laboratory/lab.js
+++ server/api/management/laboratory/lab.js
@@ -2,12 +2,49 @@ const express = require('express');
 const router = express.Router();
 const { connection } = require('../../../index');
 
+// Read all labs
+router.get("/lab", (req, res) => {
+	const sql     = `SELECT * FROM lab`;
 
+	connection.query(sql, (err, results) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(results);
+		}
+	});
+});
 
-// add a lab //TODO
-// remove a lab //TODO
-// change lab room no //TODO
+// Add a lab
+router.post("/lab", (req, res) => {
+	const { room_no } = req.body;
+    const lab_id = uuidv4();
+	const tuple = [lab_id, room_no];
+	const sql     = `INSERT INTO lab(lab_id, room_no) VALUES(?)`;
 
+	connection.query(sql, [tuple], (err, results) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(results);
+		}
+	});
+});
 
+// Remove a lab
+router.delete("/lab", async (req, res) => {
+	const { lab_id } = req.body;
+
+	const sql = `DELETE FROM lab
+				 WHERE lab_id = '${lab_id}'`;
+
+	connection.query(sql, async (err, result) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(result);
+		}
+	});
+});
 
 module.exports = router
